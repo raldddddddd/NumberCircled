c49090887fb8c8e6f29110070a7fdd7eb7632186
iofrmnowhere <gioferreras@gmail.com>
2025-05-11 15:14:11 +0800

    Uploaded Movie Review Page

---
+++ app/view/movie_review/movie_review_script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('#addReviewBtn').click(function () {
+        $('#reviewModal').css({
+          'position': 'fixed',
+          'top': '50%',
+          'left': '50%',
+          'transform': 'translate(-50%, -50%)'
+        }).fadeIn();
+      });
+
+    
+    $('#modalCloseBtn').click(function(){
+        $('#reviewModal').fadeOut();
+        $('#reviewForm')[0].reset();
+    });
+
+    $('#saveReviewBtn').click(function(e){
+    e.preventDefault();
+      var rating = $('input[name="rating"]:checked').val();
+      var review = $('#textReview').val().trim();
+
+      if (!rating || review === '') {
+        alert('Please select a rating and write a review.');
+        return;
+      }
+
+      alert(`Thanks for your review!\n\nRating: ${rating} star(s)\nReview: ${review}`);
+      $('#reviewModal').fadeOut();
+      $('#reviewForm')[0].reset();
+    });
+
+
+
+  });
