1eb593a079c822854bd348c81c988168f0dce471
raldddddddd <rissantiago31@gmail.com>
2025-05-13 14:14:13 +0800

    fixed failed fetch

--- app/controller/fetch/review_fetch.php
+++ app/controller/fetch/review_fetch.php
@@ -1,6 +1,17 @@
 <?php
 require_once __DIR__ . "/../../model/Movies.php";
 header('Content-Type: application/json');
+
+if (!isset($_GET['movie'])) {
+    echo json_encode([
+        "error" => "Missing 'movie' parameter.",
+        "movie" => [],
+        "genre" => [],
+        "review_detail" => []
+    ]);
+    exit;
+}
+
 $id = $_GET['movie'];
 $model = new Movies($conn);
 $data = [
